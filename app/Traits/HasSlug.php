a1c3fa83fb2c6e76ddc6b09ab64bb26b611ffbda
Doris Morales <dmorales55@gmail.com>
2024-07-22 21:43:16 -0500

    Manejo de las base de datos

--- app/Traits/HasSlug.php
+++ app/Traits/HasSlug.php
@@ -2,7 +2,20 @@
 
 namespace App\Traits;
 
+use Illuminate\Support\Str;
+
 trait HasSlug
 {
-  
+  public function getRouteKeyName(): string
+  {
+    return 'slug';
+  }
+
+  public static function bootHasSlug(): void
+  {
+    static::saving(function ($model)
+    {
+      $model->slug = Str::slug($model->name);
+    });
+  }
 }

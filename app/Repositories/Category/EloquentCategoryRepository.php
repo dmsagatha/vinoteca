07551b84f25064aa4af40b54e5f14a446cbf72e5
Doris Morales <dmorales55@gmail.com>
2024-07-26 16:53:07 -0500

    Repositories para la Categoria

---
+++ app/Repositories/Category/EloquentCategoryRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories\Category;
+
+class EloquentCategoryRepository implements CategoryRepositoryInterface
+{
+  /**
+   * Create a new class instance.
+   */
+  public function __construct()
+  {
+  }
+}

48a806e4b9e17084f32ad7c327fe80e44c58fc53
Doris Morales <dmorales55@gmail.com>
2024-07-25 20:58:23 -0500

    Controladores y Rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ShopController;
+use App\Http\Controllers\Wine\CategoryController;
+use App\Http\Controllers\WineController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -21,3 +25,30 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::group(['middleware' => ['auth', 'verified']], function ()
+{
+  // Route::resource('categories', CategoryController::class)->except('show');
+  Route::resource('categorias', CategoryController::class)
+    ->parameters(['categorias' => 'category'])
+    ->names('categories')
+    ->except('show');
+
+  Route::resource('wines', WineController::class)->except('show');
+
+  Route::prefix('compras')->name('shop.')->controller(ShopController::class)->group(function () {
+    Route::get('/', 'index')->name('index');
+    Route::post('adicionar-al-carro', 'addCart')->name('addCart');
+    Route::post('incrementar', 'increment')->name('increment');
+    Route::post('decrementar', 'decrement')->name('decrement');
+    Route::post('eliminar', 'remove')->name('remove');
+  });
+
+  Route::prefix('carro')->name('cart.')->controller(CartController::class)->group(function () {
+    Route::get('/', 'index')->name('index');
+    Route::post('incrementar', 'increment')->name('increment');
+    Route::post('decrementar', 'decrement')->name('decrement');
+    Route::post('eliminar', 'remove')->name('remove');
+    Route::post('limpiar', 'clear')->name('clear');
+  });
+});

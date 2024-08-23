cb7565e6e5bb3582a9d651ec613d3920dc4cb5b9
Doris Morales <dmorales55@gmail.com>
2024-08-23 11:23:01 -0500

    Interfaz del carro de compras

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Cart;
+use App\Traits\CartActions;
+use App\Repositories\Shop\ShopRepositoryInterface;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+  use CartActions;
+
+  public function __construct(private readonly ShopRepositoryInterface $repository, private readonly Cart $cart) {}
+
+  public function index(): View
+  {
+    return view('cart.index');
+  }
+
+  public function increment(): RedirectResponse
+  {
+    $this->incrementProductQuantity();
+
+    return redirect()->route('cart.index');
+  }
+
+  public function decrement(): RedirectResponse
+  {
+    $this->decrementProductQuantity();
+
+    return redirect()->route('cart.index');
+  }
+
+  public function remove(): RedirectResponse
+  {
+    $this->removeProduct();
+
+    return redirect()->route('cart.index');
+  }
+
+  public function clear(): RedirectResponse
+  {
+    $this->clearCart();
+
+    return redirect()->route('cart.index');
+  }
 }
